d830701b43573e8ffb854159f6464b35bfd9ea4e
SofiiCode <sofi.andand06@gmail.com>
2023-02-04 13:01:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var editor = new EditorJS(); /** Zero-configuration */
+
+// equals
+
+var editor = new EditorJS("editorjs");
+
+const form = document.getElementById("form");
+
+const isValidElement = (element) => {
+  return element.name && element.value;
+};
+const formToJSON = (elements) =>
+  [].reduce.call(
+    elements,
+    (data, element) => {
+      if (isValidElement(element)) {
+        data[element.name] = element.value;
+      }
+      return data;
+    },
+    {}
+  );
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const data = formToJSON(form.elements);
+  // Demo only: print the form data onscreen as a formatted JSON object.
+  const dataContainer = document.getElementsByClassName("results")[0];
+  const dataObject = JSON.stringify(data, null, "  ");
+  let response = fetch("/test.json", {
+    method: "POST",
+    body: dataObject,
+  });
+  form.reset();
+  // alert('Your data in the test.json')
+  console.log(dataObject);
+  // Use `JSON.stringify()` to make the output valid, human-readable JSON.
+  dataContainer.textContent = JSON.stringify(data, null, "  ");
+});
