868c8dd733c5cb5dbc36d153d37562802db133c7
SofiiCode <sofi.andand06@gmail.com>
2023-02-07 13:38:08 +0200

    commented editor.js

--- script.js
+++ script.js
@@ -1,96 +1,85 @@
+const form = document.getElementById("form");
 
-const editor = new EditorJS({
-  /**
-   * Id of Element that should contain Editor instance
-   */
-  holder: "editorjs",
+const isValidElement = (element) => {
+  return element.name && element.value;
+};
+const formToJSON = (elements) =>
+  [].reduce.call(
+    elements,
+    (data, element) => {
+      if (isValidElement(element)) {
+        data[element.name] = element.value;
+      }
+      return data;
+    },
+    {}
+  );
 
-  tools: {
-    header: {
-      class: Header,
-      config: {
-        levels: [2, 3, 4],
-        defaultLevel: 3,
-      },
-    },
-    paragraph: {
-      class: Paragraph,
-      inlineToolbar: true,
-    },
-  },
-  onChange: (api, event) => {
-    console.log("Now I know that Editor's content changed!", event);
-  },
-  sanitizer: {
-    // Allowed tags
-    allowedTags: ["p", "strong", "ul", "li", "ol", "a", "b", "br", "hr"],
-    // Allowed attributes
-    allowedAttributes: {
-      a: ["href", "target"],
-    },
-  },
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const data = formToJSON(form.elements);
+
+  // variable dataObject contains the data from form
+  const dataObject = JSON.stringify(data, null, "  ");
+
+  // const fs = require("fs");
+  // fs.writeFileSync("data.json", dataObject);
+
+  form.reset();
+
+  const dataContainer = document.getElementsByClassName("results")[0];
+  dataContainer.textContent = JSON.stringify(data, null, "  ");
 });
-editor.isReady
-  .then(() => {
-    console.log("Editor.js is ready to work!");
-    /** Do anything you need after editor initialization */
-  })
-  .catch((reason) => {
-    console.log(`Editor.js initialization failed because of ${reason}`);
-  });
 
+// const editor = new EditorJS({
 
- 
+//   holder: "editorjs",
 
-
-const saveButton = document.getElementById("save-button");
-    saveButton.addEventListener("click", () => {
-    editor
-      .save()
-      .then((savedData) => {
-        let savedDataJson = JSON.stringify(savedData, null, 4);
-        console.log(savedDataJson);
-      })
-      .catch((error) => {
-        console.log("Saving failed: ", error);
-      });
-    });
-
-
-
-
-
-
-// const form = document.getElementById("form");
-
-// const isValidElement = (element) => {
-//   return element.name && element.value;
-// };
-// const formToJSON = (elements) =>
-//   [].reduce.call(
-//     elements,
-//     (data, element) => {
-//       if (isValidElement(element)) {
-//         data[element.name] = element.value;
-//       }
-//       return data;
+//   tools: {
+//     header: {
+//       class: Header,
+//       config: {
+//         levels: [2, 3, 4],
+//         defaultLevel: 3,
+//       },
 //     },
-//     {}
-//   );
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   const data = formToJSON(form.elements);
-
-//   // variable dataObject contains the data from form
-//   const dataObject = JSON.stringify(data, null, "  ");
-
-//   // const fs = require("fs");
-//   // fs.writeFileSync("data.json", dataObject);
-
-//   form.reset();
-
-//   const dataContainer = document.getElementsByClassName("results")[0];
-//   dataContainer.textContent = JSON.stringify(data, null, "  ");
+//     paragraph: {
+//       class: Paragraph,
+//       inlineToolbar: true,
+//     },
+//   },
+//   onChange: (api, event) => {
+//     console.log("Now I know that Editor's content changed!", event);
+//   },
+//   sanitizer: {
+//     // Allowed tags
+//     allowedTags: ["p", "strong", "ul", "li", "ol", "a", "b", "br", "hr"],
+//     // Allowed attributes
+//     allowedAttributes: {
+//       a: ["href", "target"],
+//     },
+//   },
 // });
+
+// editor.isReady
+//   .then(() => {
+//     console.log("Editor.js is ready to work!");
+//     /** Do anything you need after editor initialization */
+//   })
+//   .catch((reason) => {
+//     console.log(`Editor.js initialization failed because of ${reason}`);
+//   });
+
+// const saveButton = document.getElementById("save-button");
+//     saveButton.addEventListener("click", () => {
+//     editor
+//       .save()
+//       .then((savedData) => {
+//         let savedDataJson = JSON.stringify(savedData, null, 4);
+//         console.log(savedDataJson);
+//       })
+//       .catch((error) => {
+//         console.log("Saving failed: ", error);
+//       });
+//     });
