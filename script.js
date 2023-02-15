37de7cb80a05fab5358792a8451727587ed3f396
SofiiCode <sofi.andand06@gmail.com>
2023-02-15 10:20:39 +0200

    rename json

--- script.js
+++ script.js
@@ -1,83 +1,84 @@
 const form = document.getElementById("form");
 
-const isValidElement = (element) => {
-  return element.name && element.value;
-};
-const formToJSON = (elements) =>
-  [].reduce.call(
-    elements,
-    (data, element) => {
-      if (isValidElement(element)) {
-        data[element.name] = element.value;
-      }
-      return data;
-    },
-    {}
-  );
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const data = formToJSON(form.elements);
-
-  // variable dataObject contains the data from form
-  const dataObject = JSON.stringify(data, null, "  ");
-
-
-  form.reset();
-
-  const dataContainer = document.getElementsByClassName("results")[0];
-  dataContainer.textContent = dataObject;
-});
-
-// const editor = new EditorJS({
-
-//   holder: "editorjs",
-
-//   tools: {
-//     header: {
-//       class: Header,
-//       config: {
-//         levels: [2, 3, 4],
-//         defaultLevel: 3,
-//       },
+// const isValidElement = (element) => {
+//   return element.name && element.value;
+// };
+// const formToJSON = (elements) =>
+//   [].reduce.call(
+//     elements,
+//     (data, element) => {
+//       if (isValidElement(element)) {
+//         data[element.name] = element.value;
+//       }
+//       return data;
 //     },
-//     paragraph: {
-//       class: Paragraph,
-//       inlineToolbar: true,
-//     },
-//   },
-//   onChange: (api, event) => {
-//     console.log("Now I know that Editor's content changed!", event);
-//   },
-//   sanitizer: {
-//     // Allowed tags
-//     allowedTags: ["p", "strong", "ul", "li", "ol", "a", "b", "br", "hr"],
-//     // Allowed attributes
-//     allowedAttributes: {
-//       a: ["href", "target"],
-//     },
-//   },
+//     {}
+//   );
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const data = formToJSON(form.elements);
+
+//   // variable dataObject contains the data from form
+//   const dataObject = JSON.stringify(data, null, "  ");
+
+
+//   form.reset();
+
+//   const dataContainer = document.getElementsByClassName("results")[0];
+//   dataContainer.textContent = dataObject;
 // });
 
-// editor.isReady
-//   .then(() => {
-//     console.log("Editor.js is ready to work!");
-//     /** Do anything you need after editor initialization */
-//   })
-//   .catch((reason) => {
-//     console.log(`Editor.js initialization failed because of ${reason}`);
-//   });
 
-// const saveButton = document.getElementById("save-button");
-//     saveButton.addEventListener("click", () => {
-//     editor
-//       .save()
-//       .then((savedData) => {
-//         let savedDataJson = JSON.stringify(savedData, null, 4);
-//         console.log(savedDataJson);
-//       })
-//       .catch((error) => {
-//         console.log("Saving failed: ", error);
-//       });
-//     });
+const saveButton = document.getElementById("save-button");
+
+const editor = new EditorJS({
+  holder: "editorjs",
+
+  tools: {
+    header: {
+      class: Header,
+    },
+
+    raw: RawTool,
+  },
+  data: {
+    blocks: [
+      {
+        type: "raw",
+        data: {
+          html: '"{"name":"","user":""},"',
+        },
+      },
+    ],
+  },
+
+  onChange: (api, event) => {
+    console.log("Now I know that Editor's content changed!", event);
+  },
+});
+
+editor.isReady
+  .then(() => {
+    console.log("Editor.js is ready to work!");
+  //  saveButton.click();
+    /** Do anything you need after editor initialization */
+  })
+  .catch((reason) => {
+    console.log(`Editor.js initialization failed because of ${reason}`);
+  });
+
+    saveButton.addEventListener("click", () => {
+       editor
+         .save()
+         .then((savedData) => {
+          let data = savedData.blocks[0].data.html;
+        
+           let savedDataJson = JSON.stringify(data, null, 4);
+           console.log(savedDataJson);
+         })
+         .catch((error) => {
+           console.log("Saving failed: ", error);
+         });
+    });
