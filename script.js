0352526cf76c31074f6d95ce98ab607f9d111ffc
SofiiCode <sofi.andand06@gmail.com>
2023-02-15 11:00:30 +0200

    show json

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 const form = document.getElementById("form");
+const url = "https://github.com/SofiiCode/form/blob/main/profile.json";
+  
+  const requestURL = "https://jsonplaceholder.typicode.com/users";
+
+  function sendRequest(method, url, body = null) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.open(method, url);
+
+      xhr.responseType = "json";
+      xhr.setRequestHeader("Content-Type", "application/json");
+
+      xhr.onload = () => {
+        if (xhr.status >= 400) {
+          reject(xhr.response);
+        } else {
+          resolve(xhr.response);
+        }
+      };
+
+      xhr.onerror = () => {
+        reject(xhr.response);
+      };
+
+      xhr.send(JSON.stringify(body));
+    });
+  }
+
+  sendRequest("GET", requestURL)
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+
+
 
 // const isValidElement = (element) => {
 //   return element.name && element.value;
